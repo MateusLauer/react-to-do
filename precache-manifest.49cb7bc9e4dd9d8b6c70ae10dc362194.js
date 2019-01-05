ec743ee25a72afc1d8ecfa480a6de4671cad2df1
Andressa <you@example.com>
2019-01-05 12:15:50 -0200

    Updates

---
+++ precache-manifest.49cb7bc9e4dd9d8b6c70ae10dc362194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de75d7bfdd984b80341b",
+    "url": "/react-to-do/static/js/runtime~main.de75d7bf.js"
+  },
+  {
+    "revision": "e154f2b4104499b4dbf4",
+    "url": "/react-to-do/static/js/main.e154f2b4.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/react-to-do/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "e154f2b4104499b4dbf4",
+    "url": "/react-to-do/static/css/main.6e30ab54.chunk.css"
+  },
+  {
+    "revision": "086f244b8281c6254c48c171ad569d4f",
+    "url": "/react-to-do/index.html"
+  }
+];
